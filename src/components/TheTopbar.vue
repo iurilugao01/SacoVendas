d198ecf1e17bd6370a58a5e17901fb037137dc74
Iuri <iurilugao01@gmail.com>
2025-04-08 09:31:13 -0300

    userinfo in topbar

--- src/components/TheTopbar.vue
+++ src/components/TheTopbar.vue
@@ -1,14 +1,38 @@
 <script setup>
+import { ref } from "vue";
+
 const userInfo = {
   username: "johndoe01",
   authority: "user",
+  profileImage:
+    "https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg",
 };
+
+const iconMouseState = ref({
+  click: false,
+  hover: false,
+});
 </script>
 
 <template>
   <div class="container">
-    <div class="user-info">
-      <span class="text-white">{{ userInfo.username }}</span>
+    <div class="user-info-container">
+      <img :src="userInfo.profileImage" alt="Foto de perfil" />
+      <div class="user-info-content text-center text-break">
+        <span class="text-white">{{ userInfo.username }}</span>
+        <span class="text-white">{{ userInfo.authority }}</span>
+      </div>
+      <i
+        :class="{
+          bi: true,
+          'bi-caret-down': !iconMouseState.hover,
+          'bi-caret-down-fill': iconMouseState.hover,
+          'rotate-icon': iconMouseState.click,
+        }"
+        @click="iconMouseState.click = !iconMouseState.click"
+        @mouseenter="iconMouseState.hover = true"
+        @mouseleave="iconMouseState.hover = false"
+      ></i>
     </div>
   </div>
 </template>
@@ -18,13 +42,38 @@ const userInfo = {
   background-color: #181717;
   position: absolute;
   display: flex;
+  flex-direction: column;
 
   top: 0;
   width: 100%;
   height: 4rem;
   margin-bottom: 12px;
 }
-.user-info {
-  right: 0;
+img {
+  width: 50px;
+  height: 50px;
+  margin-right: 12px;
+  margin-top: 8px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+.user-info-container {
+  display: flex;
+  justify-content: flex-end;
+}
+.user-info-content {
+  width: 5rem;
+  margin-top: 5px;
+}
+.bi-caret-down,
+.bi-caret-down-fill {
+  display: flex;
+  cursor: pointer;
+  height: 16px;
+  margin-top: 32px;
+  transition: transform 0.3s ease-in-out;
+}
+.rotate-icon {
+  transform: rotate(0.5turn);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <script setup>
-import AccountBox from "@/components/AccountBox.vue";
-import EntryBox from "@/components/EntryBox.vue";
-import SalesChart from "@/components/SalesChart.vue";
+// import AccountBox from "@/components/AccountBox.vue";
+// import EntryBox from "@/components/EntryBox.vue";
+// import SalesChart from "@/components/SalesChart.vue";
 </script>
 
 <template>
-  <AccountBox />
+  <p>a</p>
+  <!-- <AccountBox />
 
   <EntryBox />
 
-  <SalesChart />
+  <SalesChart /> -->
 </template>
